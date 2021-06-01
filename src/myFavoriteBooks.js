cc196e959ed13c996fdbc84df47a7cabc4535de5
heba-Abdallah <heba.essam.m@hotmail.com>
2021-06-01 19:21:44 +0300

    done lab 13 with frontend

--- src/myFavoriteBooks.js
+++ src/myFavoriteBooks.js
@@ -4,7 +4,10 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import './myFavoriteBooks.css';
 import axios from 'axios';
 import { withAuth0 } from '@auth0/auth0-react';
-import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import CardColumns from 'react-bootstrap/CardColumns';
+import FormModal from './FormModal';
 
 class MyFavoriteBooks extends React.Component {
 
@@ -12,7 +15,11 @@ class MyFavoriteBooks extends React.Component {
     super(props);
     this.state = {
       books: [],
-      showBooks: false
+      showBooks: false,
+      showModal: false,
+      bookName: '',
+      description: '',
+      image_url: ''
     }
   }
 
@@ -25,6 +32,74 @@ class MyFavoriteBooks extends React.Component {
     });
   }
 
+  handleShowModal = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+  updateBookName = (event) => {
+    this.setState({
+      bookName: event.target.value,
+
+    })
+    console.log(this.state.bookName);
+
+  }
+  updateDescription = (event) => {
+    this.setState({
+
+      description: event.target.value
+
+    })
+    console.log(this.state.description);
+  }
+  updateImgUrl = (event) => {
+    this.setState({
+      image_url: event.target.value
+    })
+    console.log(this.state.image_url);
+  }
+
+  addBooks = async (event) => {
+    event.preventDefault();
+
+    const bookFormData = {
+      name: this.state.bookName,
+      description: this.state.description,
+      image_url: this.state.image_url,
+      email: this.props.auth0.user.email
+    }
+
+    const newBooks = await axios.post('http://localhost:3001/addBooks', bookFormData)
+
+    this.setState({
+      books: newBooks.data,
+      showModal: false
+    })
+  }
+
+  deleteBook = async (index) => {
+    const email = {
+      email: this.props.auth0.user.email
+    }
+
+    let newBooks = await axios.delete(`http://localhost:3001/deleteBook/${index}`, { params: email })
+
+    this.setState({
+      books: newBooks.data
+    })
+  }
+
+
+
+
 
   render() {
     return (
@@ -33,26 +108,37 @@ class MyFavoriteBooks extends React.Component {
         <p>
           This is a collection of my favorite books
         </p>
-        <Carousel style={{width:'400px'}}>
-          {this.state.showBooks &&
-          this.state.books.map(item =>{
-            return(
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src={item.image_url}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>{item.name}</h3>
-              <p>{item.description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          )})}
-        </Carousel>
+
+        <Button variant="primary" onClick={this.handleShowModal}>➕Add a Book</Button>
+
+        {this.state.showModal && <FormModal closeModalFx={this.handleCloseModal} showModal={this.state.showModal} updateBookName={this.updateBookName} updateDescription={this.updateDescription} updateImgUrl={this.updateImgUrl} addBooks={ this.addBooks} />}
+
+        {this.state.showBooks &&
+
+          <CardColumns>
+
+            {this.state.books.map((item, idx) => {
+              return (
+                <div key={idx}>
+                  <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={item.image_url} style={{ width: '15rem', height: '16rem', margin: 'auto' }} />
+                    <Card.Body>
+                      <Card.Title>{item.name}</Card.Title>
+                      <Card.Text style={{ overflow: 'auto', height: '5rem' }}>
+                        {item.description}
+                      </Card.Text>
+                      <Button variant="primary" onClick={() => this.deleteBook(idx)}>🗑️Delete</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              )
+            })}
+          </CardColumns>
+        }
       </Jumbotron>
     )
   }
 }
+
 
 export default withAuth0(MyFavoriteBooks);
